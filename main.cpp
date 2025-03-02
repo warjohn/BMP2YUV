5a1fb4fb85e30f7055bc5182f927b5772dde92e4
Evgeniy <47078498+warjohn@users.noreply.github.com>
2025-03-02 20:27:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,46 @@
-#include "src/BMPToYUV.h"
+#include "src/ImageReader/BMPreader.h"
+#include "src/Convertor/BMPToYUV.h"
+#include "src/VideoProcess/VideoYUV.h"
+#include "src/ImageSave/SaveYUV.h"
+
 #include <iostream>
 #include <stdexcept>
 
 int main(int argc, char** argv) {
-    if (argc != 5) {
-        throw std::invalid_argument("Необходимо указать 4 аргумента. 1 - Путь до файла изображения; 2 - Путь до файла видео; 3 - Video width; 4 - Video height");
-    }
-    const std::string inputBMP = argv[1];  // Путь к BMP изображению
-    const std::string video = argv[2];
-
+    // if I can use C++23 - I'll use switch-case constructions ((
+    if (argc < 5) {
+        throw std::invalid_argument("Необходимо указать как минимум 4 аргумента: <путь_изображения> <путь_видео> <ширина_видео> <высота_видео>");
+    } 
+    // consts 
+    const std::string csinputBMP = argv[1];
+    const std::string csvideo = argv[2];
+    const int csVideoWidth = std::atoi(argv[3]);
+    const int csVideoHeight = std::atoi(argv[4]);
+  
+    // vars
+    int width, height;
+    std::vector<uint8_t> pixelData;
+    std::vector<uint8_t> yBuffer;
+    std::vector<uint8_t> uBuffer;
+    std::vector<uint8_t> vBuffer;
     const std::string outputYUV = "outputImage.yuv"; // Путь к YUV выходному файлу изображнию
     const std::string outputYUVVideo = "outputVideo.yuv"; // путь до файла видео  
 
+
+    // classes 
+    BMPreader reader(csinputBMP, width, height, pixelData);
     BMPToYUVConverter converter;
-    VideoProcessor processor;
+    VideoYUV videoYUV(csvideo, outputYUVVideo);
+    SaveYUV saveImage(outputYUV);
 
-    int width, height;
-    std::vector<uint8_t> pixelData;
-
-    if (!converter.ReadBMP(inputBMP, pixelData, width, height)) {
-        return -1;
-    }
-
-    std::cout << "Image shape - " << width << "x" << height << std::endl;
-
-    size_t ySize = width * height;
-    size_t uvSize = (width / 2) * (height / 2);
-
-    std::vector<uint8_t> yBuffer(ySize);
-    std::vector<uint8_t> uBuffer(uvSize);
-    std::vector<uint8_t> vBuffer(uvSize);
-
+    // process
+    reader.read();
+    if (width > csVideoWidth || height > csVideoHeight) { 
+        throw std::runtime_error("Pls use images with small or same size like a video. Resize function will be add in future addons");
+    } 
     converter.ConvertBGRToYUV420P(pixelData, width, height, yBuffer, uBuffer, vBuffer);
-    converter.InvertYUVVertically(yBuffer, uBuffer, vBuffer, width, height);
-
-    const int VideoWidth = std::atoi(argv[3]);  // Узнал из просмоторщика vooya 352x288 - тестовое видео
-    const int VideoHeight = std::atoi(argv[4]);
-
-    processor.ProcessVideo(video, outputYUVVideo, VideoWidth, VideoHeight, yBuffer, uBuffer, vBuffer, width, height);
-
-    converter.SaveYUV(outputYUV, yBuffer, uBuffer, vBuffer, width, height);
+    videoYUV.ProcessVideo(csVideoWidth, csVideoHeight, yBuffer, uBuffer, vBuffer, width, height);
+    saveImage.save(yBuffer, uBuffer, vBuffer, width, height);
     std::cout << "YUV файл успешно сохранен!" << std::endl;
 
     return 0;
